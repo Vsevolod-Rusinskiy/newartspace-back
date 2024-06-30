92e3bdd1757e2e74ad4638476fe25f6158a75993
vsevolod <leto9999@gmail.com>
2024-06-30 12:33:32 +0300

    allow CORS

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,18 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    },
-    methods: 'GET,POST,PUT,DELETE,OPTIONS, PATCH',
-    allowedHeaders: 'Content-Type, Authorization',
-  })
+  app.enableCors()
+  // app.enableCors({
+  //   origin: function (origin, callback) {
+  //     if (allowedOrigins.includes(origin) || !origin) {
+  //       callback(null, true)
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'))
+  //     }
+  //   },
+  //   methods: 'GET,POST,PUT,DELETE,OPTIONS, PATCH',
+  //   allowedHeaders: 'Content-Type, Authorization',
+  // })
   await app.listen(process.env.PORT || 3000)
 }
 
